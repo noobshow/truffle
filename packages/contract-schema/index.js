46ead4ef6a960b665807b74a16e137dfa6857828
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2021-03-03 13:57:51 +0000

    Add architecture to schema.

--- packages/contract-schema/index.js
+++ packages/contract-schema/index.js
@@ -59,6 +59,9 @@ const sanitizeAllEvents = dirtyEvents =>
   );
 
 var properties = {
+  architecture: {
+    sources: ["architecture"]
+  },
   contractName: {
     sources: ["contractName", "contract_name"]
   },
