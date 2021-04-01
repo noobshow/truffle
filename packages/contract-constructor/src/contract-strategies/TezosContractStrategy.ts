d9ee28973232fa29cbdb9877fe614632a3b19a44
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2021-04-01 18:48:48 +0100

    estimateGas

--- packages/contract-constructor/src/contract-strategies/TezosContractStrategy.ts
+++ packages/contract-constructor/src/contract-strategies/TezosContractStrategy.ts
@@ -41,21 +41,12 @@ export class TezosContractStrategy implements IContractStrategy {
     };
   }
 
-  deploy(txArguments: any[], txParams: TxParams): Promise<ContractInstance> {
+  deploy(txArgs: any[], txParams: TxParams): Promise<ContractInstance> {
     const promiEvent = Web3PromiEvent();
 
-    const michelson: any = JSON.parse(this._json.michelson);
+    const originateOp = this.originateParams(txArgs, txParams);
 
-    const originateParams = {
-      code: michelson,
-      storage: txArguments[0],
-      balance: txParams.value as string | "0",
-      fee: txParams.fee,
-      gasLimit: txParams.gas, // TODO BGC Should we use gasLimit instead?
-      storageLimit: txParams.storageLimit
-    };
-
-    this.interfaceAdapter.tezos.contract.originate(originateParams)
+    this.interfaceAdapter.tezos.contract.originate(originateOp)
       .then((receipt) => {
         return receipt.contract();
       })
@@ -116,56 +107,26 @@ export class TezosContractStrategy implements IContractStrategy {
     throw new Error("Method not implemented.");
   }
 
-  estimateGas(/*contractInstance: ContractAbstraction<ContractProvider>, txArgs: any[], txParams: { [key: string]: any }*/) {
-    // import { TezosToolkit } from '@taquito/taquito';
-    // const Tezos = new TezosToolkit('https://api.tez.ie/rpc/edonet');
-
-    // Tezos.contract
-    // .at('KT1MZR1g3jZCU6itoyEZ7u91hyJMG2efgmwu')
-    // .then((contract) => {
-    //   const i = 7;
-
-    //   return contract.methods.increment(i).toTransferParams({});
-    // })
-    // .then((op) => {
-    //   println(`Estimating the smart contract call : `);
-    //   return Tezos.estimate.transfer(op);
-    // })
-    // .then((est) => {
-    //   println(`burnFeeMutez : ${est.burnFeeMutez}, 
-    //   gasLimit : ${est.gasLimit}, 
-    //   minimalFeeMutez : ${est.minimalFeeMutez}, 
-    //   storageLimit : ${est.storageLimit}, 
-    //   suggestedFeeMutez : ${est.suggestedFeeMutez}, 
-    //   totalCost : ${est.totalCost}, 
-    //   usingBaseFeeMutez : ${est.usingBaseFeeMutez}`);
-    // })
-    // .catch((error) => console.table(`Error: ${JSON.stringify(error, null, 2)}`));
+  async estimateGas(contractInstance: ContractAbstraction<ContractProvider>, method: string, txArgs: any[], _txParams: { [key: string]: any }) {
+    const transferOp = contractInstance.methods[method](txArgs).toTransferParams({});
+    return this.interfaceAdapter.tezos.estimate.transfer(transferOp);
   }
 
-  estimateGasNew() {
-    // import { TezosToolkit } from '@taquito/taquito';
-    // const Tezos = new TezosToolkit('https://api.tez.ie/rpc/edonet');
+  async estimateGasNew(txArgs: any[], txParams: { [key: string]: any }) {
+    const originateOp = this.originateParams(txArgs, txParams);
+    return this.interfaceAdapter.tezos.estimate.originate(originateOp);
+  }
 
-    // println(`Estimating the contract origination : `);
-    // Tezos.estimate
-    //   .originate({
-    //     code: genericMultisigJSONfile,
-    //     storage: {
-    //       stored_counter: 0,
-    //       threshold: 1,
-    //       keys: ['edpkuLxx9PQD8fZ45eUzrK3BhfDZJHhBuK4Zi49DcEGANwd2rpX82t'],
-    //     },
-    //   })
-    //   .then((originationOp) => {
-    //     println(`burnFeeMutez : ${originationOp.burnFeeMutez}, 
-    //     gasLimit : ${originationOp.gasLimit}, 
-    //     minimalFeeMutez : ${originationOp.minimalFeeMutez}, 
-    //     storageLimit : ${originationOp.storageLimit}, 
-    //     suggestedFeeMutez : ${originationOp.suggestedFeeMutez}, 
-    //     totalCost : ${originationOp.totalCost}, 
-    //     usingBaseFeeMutez : ${originationOp.usingBaseFeeMutez}`);
-    //   })
-    //   .catch((error) => println(`Error: ${JSON.stringify(error, null, 2)}`));
+  private originateParams(txArgs: any[], txParams: { [key: string]: any }) {
+    const michelson: any = JSON.parse(this._json.michelson);
+
+    return {
+      code: michelson,
+      storage: txArgs[0],
+      balance: txParams.value as string | "0",
+      fee: txParams.fee,
+      gasLimit: txParams.gas, // TODO BGC Should we use gasLimit instead?
+      storageLimit: txParams.storageLimit
+    };
   }
 }
